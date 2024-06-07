5167fdc399968760ab34ea07ffa4d105cbfb8b9e
chihabMe <chihab.mg.me@gmail.com>
2024-06-07 19:29:50 +0100

    fixed build problems

--- src/components/home/Intro.tsx
+++ src/components/home/Intro.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Button from "../ui/Button";
 
 const Intro = () => {
@@ -6,7 +5,7 @@ const Intro = () => {
     <section className="relative w-full max-w-[1800px] mx-auto">
       <img src="/images/home/element.png" className=" top-1 left-1 absolute" />
       <div className="container  mx-auto  flex items-center justify-between  min-h-[78vh] pt-10">
-        <dev className="max-w-screen-sm  ">
+        <div className="max-w-screen-sm  ">
           <h1 className="text-5xl font-semibold text-lg py-2">
             Virtual healthcare
           </h1>
@@ -16,10 +15,10 @@ const Intro = () => {
             accessible on mobile and online for everyone
           </p>
           <Button className="my-10 font-bold py-4">Consult today</Button>
-        </dev>
-        <dev>
+        </div>
+        <div>
           <img src="/images/home/intro.png" />
-        </dev>
+        </div>
       </div>
     </section>
   );
